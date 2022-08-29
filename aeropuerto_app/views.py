a960cfa8c1da2d4eee9f60812d46f4a91f6573b9
Rene Arrieta <renearrieta8905@gmail.com>
2022-08-29 10:04:24 -0500

    Ago 25 - Part 1

--- aeropuerto_app/views.py
+++ aeropuerto_app/views.py
@@ -8,3 +8,24 @@ from rest_framework import viewsets, status
 class Base_view(viewsets.ModelViewSet):
     queryset = Base.objects.all()
     serializer_class = Base_Serializer
+
+class Piloto_view(viewsets.ModelViewSet):
+    queryset = Piloto.objects.all()
+    serializer_class = Piloto_Serializer
+
+class Avion_view(viewsets.ModelViewSet):
+    queryset = Avion.objects.all()
+    serializer_class = Avion_Serializer
+
+class Miembro_view(viewsets.ModelViewSet):
+    queryset = Miembro.objects.all()
+    serializer_class = Miembro_Serializer
+
+class Tripulacion_view(viewsets.ModelViewSet):
+    queryset = Tripulacion.objects.all()
+    serializer_class = Tripulacion_Serializer
+
+class Vuelo_view(viewsets.ModelViewSet):
+    queryset = Vuelo.objects.all()
+    serializer_class = Vuelo_Serializer
+
